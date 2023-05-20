e4c618bd0ee1e1821acabf2f2eca0d5f2667e4f1
Maria Popova <0979867251m@gmail.com>
2023-05-21 02:53:09 +0300

    card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
+import { useState, useCallback, useEffect, useMemo } from 'react';
+import { getTweets, updateTweets } from 'services/Api';
+import { CardList } from './CardList/CardList';
+import { LoadMore } from './LoadMore/LoadMore';
+import { scrollOnLoadMore } from 'utils/scrollOnLoadMore';
 export const App = () => {
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const [showLoadMore, setShowLoadMore] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      try {
+        const { data } = await getTweets();
+        setUsers(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
+  const handleFollowing = useCallback((id, following) => {
+    try {
+      setUsers(prevUsers =>
+        prevUsers.map(user => {
+          if (user.id === id) {
+            const updatedFollowers = following
+              ? user.followers - 1
+              : user.followers + 1;
+
+            const updatedUser = {
+              ...user,
+              followers: updatedFollowers,
+              following: !user.following,
+            };
+
+            updateTweets(id, updatedFollowers, updatedUser.following);
+
+            return updatedUser;
+          }
+          return user;
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const filteredUsers = useMemo(() => {
+    let filtered = users;
+
+    const endIndex = page * 3;
+    setShowLoadMore(endIndex);
+    return filtered.slice(0, endIndex);
+  }, [page, users]);
+
+  const handleBtnLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+    scrollOnLoadMore();
+  };
+
+  const isLoadMoreVisible = showLoadMore === filteredUsers.length;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <CardList users={filteredUsers} onClick={handleFollowing} />
+      {isLoadMoreVisible && (
+        <LoadMore handleBtnLoadMore={handleBtnLoadMore} disabled={isLoading} />
+      )}
     </div>
   );
 };
