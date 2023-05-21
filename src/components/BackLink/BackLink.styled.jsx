a2bee8d54af7188464e901d52d9ed41ea4963020
Maria Popova <0979867251m@gmail.com>
2023-05-21 20:41:31 +0300

    finish

--- src/components/BackLink/BackLink.styled.jsx
+++ src/components/BackLink/BackLink.styled.jsx
@@ -8,12 +8,7 @@ export const StyledLink = styled(Link)`
   margin-bottom: 20px;
   padding: 8px;
   text-decoration: none;
-  font-family: 'Montserrat';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 18px;
-  line-height: 22px;
-  text-transform: uppercase;
+
   border-radius: 5px;
   color: #373737;
   background-color: #ebd8ff;
