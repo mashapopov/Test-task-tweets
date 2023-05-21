6f4f4d02c9422a233ccbbb4dcd924d0cd1e8fd3c
Maria Popova <0979867251m@gmail.com>
2023-05-21 16:30:38 +0300

    finish

--- src/components/LoadMore/LoadMore.styled.jsx
+++ src/components/LoadMore/LoadMore.styled.jsx
@@ -10,8 +10,16 @@ export const Button = styled.button`
   border: none;
 
   transition: background-color 500ms cubic-bezier(0.075, 0.82, 0.165, 1);
-
-  :hover {
-    background-color: #ebd8ffae;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 22px;
+  text-transform: uppercase;
+  color: #373737;
+  background-color: #ebd8ff;
+  &:hover,
+  &:focus {
+    background-color: #5cd3a8;
   }
 `;
